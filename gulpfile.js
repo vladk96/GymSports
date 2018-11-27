9e6146998bacc7dd760050c7bf23e950a792f8af
vladk96 <vladk961@gmail.com>
2018-11-27 19:23:50 +0200

    create project structure

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('serve', ['sass'], function() {
         server: "src/"
     });
 
-    gulp.watch("src/sass/*.scss", ['sass']);
+    gulp.watch("src/scss/*.scss", ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
